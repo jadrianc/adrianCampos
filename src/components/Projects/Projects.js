26bb8764a611200037c864e8cdb397f66e82de89
Adrian Campos <68518108+jadrianc@users.noreply.github.com>
2022-08-23 16:51:44 -0600

    Adding project section

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -5,9 +5,38 @@ import { Section, SectionDivider, SectionTitle } from '../../styles/GlobalCompon
 import { projects } from '../../constants/constants';
 
 const Projects = () => (
-  <div>
-    Projects
-  </div>
+  <Section nopadding id='projects' >
+    <SectionDivider />
+    <SectionTitle main>Projects</SectionTitle>
+    <GridContainer>
+      { 
+        projects.map((project) => ( 
+          <BlogCard key={project.id}>
+            <Img src={project.image} alt={project.title} />
+            <TitleContent>
+              <HeaderThree title>{project.title}</HeaderThree>
+              <Hr />
+            </TitleContent>
+            <CardInfo>{project.description}</CardInfo>
+            <div>
+              <TitleContent>Stack</TitleContent>
+              <TagList>
+                {
+                  project.tags.map((tag) => (
+                    <Tag key={tag}>{tag}</Tag>
+                  ))
+                }
+              </TagList>
+            </div>
+            <UtilityList>
+              <ExternalLinks href={project.visit}>Code</ExternalLinks>
+              <ExternalLinks href={project.source}>Source</ExternalLinks>
+            </UtilityList>
+          </BlogCard>
+        ))
+      }
+    </GridContainer>
+  </Section>
 );
 
 export default Projects;
