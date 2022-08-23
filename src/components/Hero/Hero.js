1030bdbf75f15ce963a59d63cfd9011e2e1e629a
Adrian Campos <68518108+jadrianc@users.noreply.github.com>
2022-08-23 16:22:07 -0600

    Adding header section to the project

---
+++ src/components/Hero/Hero.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+import { Section, SectionText, SectionTitle } from '../../styles/GlobalComponents';
+import Button from '../../styles/GlobalComponents/Button';
+import { LeftSection } from './HeroStyles';
+
+const Hero = (props) => (
+  <Section row nopadding>
+    <LeftSection>
+      <SectionTitle main center>
+        Welcome to <br />
+        My personal Portfolio
+      </SectionTitle>
+      <SectionText>
+        the pourpose of this website is to showcase my skills and projects.
+      </SectionText>
+      <Button onClick={{}}>Contact Me</Button>
+    </LeftSection>
+  </Section>
+);
+
+export default Hero;
